a5ebb2bc3e8ed0887345cfc45abea82158ba842d
Andrii <kostuchenkojr@gmail.com>
2025-10-31 03:11:09 +0200

    compleat sql feature

--- src/TollService.Infrastructure/Services/SpatialQueryService.cs
+++ src/TollService.Infrastructure/Services/SpatialQueryService.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
 using TollService.Application.Common.Interfaces;
 using TollService.Infrastructure.Persistence;
 
@@ -16,18 +15,18 @@ public class SpatialQueryService : ISpatialQueryService
 
     public async Task<double?> GetRoadLengthAsync(Guid roadId, CancellationToken ct = default)
     {
-        var roadIdParam = new NpgsqlParameter("roadId", roadId);
-        
+        FormattableString sql = $"""
+            SELECT ST_LengthSpheroid(
+                "Geometry",
+                'SPHEROID["WGS 84",6378137,298.257223563]'::spheroid
+            ) / 1000.0 AS "Value"
+            FROM "Roads"
+            WHERE "Id" = {roadId}::uuid
+            AND "Geometry" IS NOT NULL
+            """;
+
         var length = await _context.Database
-            .SqlQueryRaw<double?>("""
-                SELECT ST_LengthSpheroid(
-                    geometry::geometry,
-                    'SPHEROID["WGS 84",6378137,298.257223563]'::spheroid
-                ) / 1000.0 as LengthKm
-                FROM "Roads"
-                WHERE "Id" = @roadId::uuid
-                AND geometry IS NOT NULL
-                """, roadIdParam)
+            .SqlQuery<double?>(sql)
             .FirstOrDefaultAsync(ct);
 
         return length;
@@ -35,21 +34,21 @@ public class SpatialQueryService : ISpatialQueryService
 
     public async Task<double> GetTotalRoadDistanceByStateAsync(string stateCode, CancellationToken ct = default)
     {
-        var stateParam = new NpgsqlParameter("state", stateCode);
-        
+        FormattableString sql = $"""
+            SELECT COALESCE(
+                SUM(ST_LengthSpheroid(
+                    "Geometry",
+                    'SPHEROID["WGS 84",6378137,298.257223563]'::spheroid
+                )) / 1000.0,
+                0.0
+            ) AS "Value"
+            FROM "Roads"
+            WHERE "State" = {stateCode}
+            AND "Geometry" IS NOT NULL
+            """;
+
         var totalLength = await _context.Database
-            .SqlQueryRaw<double>("""
-                SELECT COALESCE(
-                    SUM(ST_LengthSpheroid(
-                        geometry::geometry,
-                        'SPHEROID["WGS 84",6378137,298.257223563]'::spheroid
-                    )) / 1000.0, 
-                    0.0
-                ) as TotalLengthKm
-                FROM "Roads"
-                WHERE "State" = @state
-                AND geometry IS NOT NULL
-                """, stateParam)
+            .SqlQuery<double>(sql)
             .FirstOrDefaultAsync(ct);
 
         return totalLength;
