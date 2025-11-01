41fbb9862c8cc0e2d73e16dae612a981d3b9ac89
Andrii <kostuchenkojr@gmail.com>
2025-11-01 23:38:06 +0200

    add-migration

---
+++ src/TollService.Infrastructure/Migrations/20251101212730_Add-WayId_route.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TollService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWayId_route : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "WayId",
+                table: "Roads",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WayId",
+                table: "Roads");
+        }
+    }
+}
