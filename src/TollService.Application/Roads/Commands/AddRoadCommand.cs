be1e57dfa57ea5957b61e4b4bc8250a808a780f4
Andrii <kostuchenkojr@gmail.com>
2025-11-06 23:41:08 +0200

    add cords to AddRoadCommandHandler

--- src/TollService.Application/Roads/Commands/AddRoadCommand.cs
+++ src/TollService.Application/Roads/Commands/AddRoadCommand.cs
@@ -1,12 +1,13 @@
 using AutoMapper;
 using MediatR;
+using NetTopologySuite.Geometries;
 using TollService.Contracts;
 using TollService.Domain;
 using TollService.Application.Common.Interfaces;
 
 namespace TollService.Application.Roads.Commands;
 
-public record AddRoadCommand(string Name, string HighwayType, bool IsToll) : IRequest<RoadDto>;
+public record AddRoadCommand(string Name, string HighwayType, bool IsToll, List<(double Latitude, double Longitude)>? Coordinates = null) : IRequest<RoadDto>;
 
 public class AddRoadCommandHandler(
     IMapper _mapper,
@@ -14,12 +15,23 @@ public class AddRoadCommandHandler(
 {
     public async Task<RoadDto> Handle(AddRoadCommand request, CancellationToken ct)
     {
+        LineString? geometry = null;
+        
+        if (request.Coordinates != null && request.Coordinates.Count >= 2)
+        {
+            var coords = request.Coordinates
+                .Select(c => new Coordinate(c.Longitude, c.Latitude))
+                .ToArray();
+            geometry = new LineString(coords) { SRID = 4326 };
+        }
+
         var road = new Road
         {
             Id = Guid.NewGuid(),
             Name = request.Name,
             HighwayType = request.HighwayType,
-            IsToll = request.IsToll
+            IsToll = request.IsToll,
+            Geometry = geometry
         };
 
         _context.Roads.Add(road);
